4fe5827d0dd8483ad8e043b551d412546c7b4770
iriapantazi <iria.a.pantazi@gmail.com>
2025-07-29 16:53:01 +0100

    add librispeech and fix config

--- training/example_train.sh
+++ training/example_train.sh
@@ -16,8 +16,7 @@ cat configs/${CONFIG_NAME}.yaml |
 	sed "s|SENTENCEPIECE|${DATASET_NAME_LOWER_CASE}${SPM_SIZE}|" |
 	sed "s|STATS_SUBDIR|${DATASET_NAME_LOWER_CASE}-winsz${WINSZ}|" |
 	sed "s/NGRAM_SUBDIR/${DATASET_NAME_LOWER_CASE}${SPM_SIZE}/" |
-	sed "s/MAX_DURATION/${MAX_DURATION_SECS}/" |
-	sed "s/WORDS/words/" > \
+	sed "s/MAX_DURATION/${MAX_DURATION_SECS}/" > \
 		/tmp/${CONFIG_NAME}.yaml
 
 # Set as appropriate for machine
@@ -27,14 +26,14 @@ BSF=1
 NUM_GPUS=$(nvidia-smi -L | wc -l)
 
 ./scripts/train.sh \
-	--train_manifests librispeech-train-clean-100-flac.eos.json \
+	--train_manifests librispeech-dev-clean-flac.json \
 	--model_config /tmp/${CONFIG_NAME}.yaml \
 	--num_gpus "$NUM_GPUS" \
 	--global_batch_size $GBS \
 	--grad_accumulation_batches $GAB \
 	--batch_split_factor $BSF \
-	--val_manifests librispeech-dev-other-flac.json \
-	--data_dir /datasets/OpenSourceEOS/ \
+	--val_manifests librispeech-dev-clean-flac.json \
+	--data_dir /datasets/LibriSpeech/ \
 	--skip_state_dict_check \
 	--training_steps 20 \
 	--calculate_emission_latency \
--- training/pkg_install.sh
+++ training/pkg_install.sh
@@ -9,3 +9,9 @@ cd ../
 pip install --disable-pip-version-check -U -r requirements.txt
 python -m pip install -e .
 pip install --no-dependencies torchdata==0.6.1
+python ./caiman_asr_train/data/make_datasets/librispeech.py --dataset_parts dev-clean
+mkdir /datasets/mel_stats
+mkdir /datasets/sentencepieces
+cp open1.8-beta1023.model /datasets/sentencepieces/.
+cp open1.8-beta1023.vocab /datasets/sentencepieces/.
+cp -r open1.8-beta-winsz0.02 /datasets/mel_stats/.
